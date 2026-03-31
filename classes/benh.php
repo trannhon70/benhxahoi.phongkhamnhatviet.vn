02ce2203923a62eb0001343d83cac11f136d9e95
trannhon70 <kevintran351996@gmail.com>
2026-03-31 17:08:49 +0700

    update

--- classes/benh.php
+++ classes/benh.php
@@ -5,72 +5,74 @@ include_once($filepath . '/../lib/database.php');
 include_once($filepath . '/../helpers/format.php');
 ?>
 
-<?php 
-  class Benh 
+<?php
+class Benh
+{
+  private $db;
+  private $fm;
+  public function __construct()
   {
-      private $db;
-      private $fm;
-      public function __construct()
-      {
-          $this->db = new Database();
-          $this->fm = new Format();
-      }
+    $this->db = new Database();
+    $this->fm = new Format();
+  }
 
-      //thêm danh mục 
-      public function getByIdKhoa(){
-        $query = "SELECT * FROM admin_benh WHERE id_khoa = 1 ";
-        $result = $this->db->select($query);
-        return $result;
-        
-      }
-      
-      public function getAllDanhSachBenh(){
-        $query = "SELECT * FROM admin_benh WHERE 1";
-        $result = $this->db->select($query);
-        return $result;
-        
-      }
+  //thêm danh mục 
+  public function getByIdKhoa()
+  {
+    $query = "SELECT * FROM admin_benh";
+    $result = $this->db->select($query);
+    return $result;
+  }
 
-      function getDanhSachBenhByIdKhoa($idKhoa){
-        $query = "SELECT * FROM `admin_benh` WHERE id_khoa = '$idKhoa' AND hidden = '0' ";
+  public function getAllDanhSachBenh()
+  {
+    $query = "SELECT * FROM admin_benh WHERE 1";
+    $result = $this->db->select($query);
+    return $result;
+  }
+
+  function getDanhSachBenhByIdKhoa($idKhoa)
+  {
+    $query = "SELECT * FROM `admin_benh` WHERE id_khoa = '$idKhoa' AND hidden = '0' ";
+    $result = $this->db->select($query);
+
+    $data = [];
+    if ($result) {
+      while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+      }
+    }
+    return $data;
+  }
+
+  public function getMenuMobile()
+  {
+    $queryKhoa = "SELECT * FROM `admin_khoa` WHERE `id` BETWEEN 1 AND 4";
+    $resultKhoa = $this->db->select($queryKhoa);
+    $data = [];
+    if ($resultKhoa) {
+      while ($row = $resultKhoa->fetch_assoc()) {
+        $idKhoa = $row['id'];
+
+        $query = "SELECT * FROM `admin_benh` WHERE `id_khoa` = '$idKhoa'";
         $result = $this->db->select($query);
-        
-        $data = [];
+
+        $dsBenh = [];
         if ($result) {
-            while ($row = $result->fetch_assoc()) {
-                $data[] = $row;
-            }
+          while ($benhRow = $result->fetch_assoc()) {
+            $dsBenh[] = $benhRow;
+          }
         }
-        return $data;
-      }
 
-      public function getMenuMobile() {
-        $queryKhoa = "SELECT * FROM `admin_khoa` WHERE `id` BETWEEN 1 AND 4";
-        $resultKhoa = $this->db->select($queryKhoa);
-        $data = [];
-        if ($resultKhoa) {
-            while ($row = $resultKhoa->fetch_assoc()) {
-                $idKhoa = $row['id'];
-                
-                $query = "SELECT * FROM `admin_benh` WHERE `id_khoa` = '$idKhoa'";
-                $result = $this->db->select($query);
-    
-                $dsBenh = [];
-                if ($result) {
-                    while ($benhRow = $result->fetch_assoc()) {
-                        $dsBenh[] = $benhRow;
-                    }
-                }
-    
-                $row['dsBenh'] = $dsBenh;
-                $data[] = $row;
-            }
-        }
-    
-        return $data;
+        $row['dsBenh'] = $dsBenh;
+        $data[] = $row;
+      }
     }
 
-    public function getActiveByBenh($path)
+    return $data;
+  }
+
+  public function getActiveByBenh($path)
   {
     $path = mysqli_real_escape_string($this->db->link, $path);
     $queryBenh = "SELECT id_benh FROM `admin_baiviet` WHERE slug = '$path' LIMIT 1";
@@ -90,6 +92,6 @@ include_once($filepath . '/../helpers/format.php');
 
     return null;
   }
-  }
-  
+}
+
 ?>
--- classes/khoa.php
+++ classes/khoa.php
@@ -20,7 +20,7 @@ class khoa
   public function getAllKhoa()
   {
 
-    $query = "SELECT * FROM `admin_khoa` WHERE id = 1 ";
+    $query = "SELECT * FROM `admin_khoa`";
     $result = $this->db->select($query);
     return $result;
   }
@@ -104,73 +104,75 @@ class khoa
     return $data;
   }
 
-  public function getTTBenhAndKhoa($khoa_slug, $benh_slug){
+  public function getTTBenhAndKhoa($khoa_slug, $benh_slug)
+  {
     // Sử dụng mysqli_real_escape_string để tránh SQL injection
     $khoa_slug = mysqli_real_escape_string($this->db->link, $khoa_slug);
     $benh_slug = mysqli_real_escape_string($this->db->link, $benh_slug);
 
     $queryKhoa = "SELECT name FROM `admin_khoa` WHERE slug = '$khoa_slug' LIMIT 1";
     $resultKhoa = $this->db->select($queryKhoa);
-    $data = []; 
+    $data = [];
     if ($resultKhoa) {
-        $khoa = $resultKhoa->fetch_assoc();  
-        $queryBenh = "SELECT name FROM `admin_benh` WHERE slug = '$benh_slug' LIMIT 1";
-        $resultBenh = $this->db->select($queryBenh);
-        if ($resultBenh) {
-            $benh = $resultBenh->fetch_assoc(); 
-            $data = [
-                'khoa' => $khoa['name'],  
-                'benh' => $benh['name']  
-            ];
-        }
+      $khoa = $resultKhoa->fetch_assoc();
+      $queryBenh = "SELECT name FROM `admin_benh` WHERE slug = '$benh_slug' LIMIT 1";
+      $resultBenh = $this->db->select($queryBenh);
+      if ($resultBenh) {
+        $benh = $resultBenh->fetch_assoc();
+        $data = [
+          'khoa' => $khoa['name'],
+          'benh' => $benh['name']
+        ];
+      }
     }
 
-    return $data;  
-}
-
-public function getDanhMucBenhByBaiViet($slug){
-  // Xử lý input để tránh lỗi SQL Injection
-  $slug = mysqli_real_escape_string($this->db->link, $slug);
-  // Truy vấn để lấy id_khoa từ bảng admin_baiviet dựa trên slug
-  $queryBaiViet = "SELECT id_khoa FROM `admin_baiviet` WHERE slug = '$slug' LIMIT 1";
-  $resultBaiViet = $this->db->select($queryBaiViet);
-  
-  $data = []; // Mảng lưu trữ kết quả cuối cùng
-
-  if($resultBaiViet){
-      // Duyệt qua từng bản ghi của bảng admin_baiviet (nếu có nhiều bản ghi)
-      while($rowBaiViet = $resultBaiViet->fetch_assoc()){
-          $id = $rowBaiViet['id_khoa'];
-          // Truy vấn lấy thông tin khoa từ bảng admin_khoa dựa trên id_khoa
-          $queryKhoa = "SELECT * FROM `admin_khoa` WHERE id = '$id' LIMIT 1";
-          $resultKhoa = $this->db->select($queryKhoa);
-          
-          if($resultKhoa){
-              while($rowKhoa = $resultKhoa->fetch_assoc()){
-                  $idKhoa = $rowKhoa['id'];
-                  // Truy vấn lấy danh sách bệnh từ bảng admin_benh dựa trên id_khoa
-                  $danhSachBenh = "SELECT * FROM `admin_benh` WHERE id_khoa = '$idKhoa'";
-                  $resultDSBenh = $this->db->select($danhSachBenh);
-                  
-                  // Lưu danh sách bệnh vào mảng
-                  $danhSachBenhArr = [];
-                  if($resultDSBenh){
-                      while($rowBenh = $resultDSBenh->fetch_assoc()){
-                          $danhSachBenhArr[] = $rowBenh;
-                      }
-                  }
-                  
-                  // Gán danh sách bệnh vào mảng kết quả khoa
-                  $rowKhoa['danhSachBenh'] = $danhSachBenhArr;
-                  $data[] = $rowKhoa;
-              }
-          }
-      }   
+    return $data;
   }
 
-  // Trả về mảng kết quả cuối cùng
-  return $data;
-}
+  public function getDanhMucBenhByBaiViet($slug)
+  {
+    // Xử lý input để tránh lỗi SQL Injection
+    $slug = mysqli_real_escape_string($this->db->link, $slug);
+    // Truy vấn để lấy id_khoa từ bảng admin_baiviet dựa trên slug
+    $queryBaiViet = "SELECT id_khoa FROM `admin_baiviet` WHERE slug = '$slug' LIMIT 1";
+    $resultBaiViet = $this->db->select($queryBaiViet);
+
+    $data = []; // Mảng lưu trữ kết quả cuối cùng
+
+    if ($resultBaiViet) {
+      // Duyệt qua từng bản ghi của bảng admin_baiviet (nếu có nhiều bản ghi)
+      while ($rowBaiViet = $resultBaiViet->fetch_assoc()) {
+        $id = $rowBaiViet['id_khoa'];
+        // Truy vấn lấy thông tin khoa từ bảng admin_khoa dựa trên id_khoa
+        $queryKhoa = "SELECT * FROM `admin_khoa` WHERE id = '$id' LIMIT 1";
+        $resultKhoa = $this->db->select($queryKhoa);
+
+        if ($resultKhoa) {
+          while ($rowKhoa = $resultKhoa->fetch_assoc()) {
+            $idKhoa = $rowKhoa['id'];
+            // Truy vấn lấy danh sách bệnh từ bảng admin_benh dựa trên id_khoa
+            $danhSachBenh = "SELECT * FROM `admin_benh` WHERE id_khoa = '$idKhoa'";
+            $resultDSBenh = $this->db->select($danhSachBenh);
+
+            // Lưu danh sách bệnh vào mảng
+            $danhSachBenhArr = [];
+            if ($resultDSBenh) {
+              while ($rowBenh = $resultDSBenh->fetch_assoc()) {
+                $danhSachBenhArr[] = $rowBenh;
+              }
+            }
+
+            // Gán danh sách bệnh vào mảng kết quả khoa
+            $rowKhoa['danhSachBenh'] = $danhSachBenhArr;
+            $data[] = $rowKhoa;
+          }
+        }
+      }
+    }
+
+    // Trả về mảng kết quả cuối cùng
+    return $data;
+  }
 }
 
 ?>
